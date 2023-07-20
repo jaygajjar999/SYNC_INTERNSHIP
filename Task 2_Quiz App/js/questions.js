717270c745d3367f6fe1cff553a7643b2c68feb5
jaygajjar999 <108823600+jaygajjar999@users.noreply.github.com>
2023-07-20 15:12:23 +0530

    Add files via upload

---
+++ Task 2_Quiz App/js/questions.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+    numb: 1,
+    question: "What does CPU stand for?",
+    answer: "Central Processing Unit",
+    options: [
+      "Central Processing Unit",
+      "Computer Power Unit",
+      "Central Power Unit",
+      "Computer Processing Unit"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Which programming language is primarily used for web development?",
+    answer: "HTML",
+    options: [
+      " Java",
+      "Python",
+      "HTML",
+      "C++"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What does RAM stand for?",
+    answer: "Random Access Memory",
+    options: [
+      "Random Access Memory",
+      "Read-Only Memory",
+      "Real-time Access Module",
+      "Randomized Allocation Memory"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Which of the following is an input device?",
+    answer: "Keyboard",
+    options: [
+      "Monitor",
+      "Keyboard",
+      "Printer",
+      "Speaker"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What is the function of an operating system in a computer?",
+    answer: "To manage hardware and software resourcese",
+    options: [
+      "To process data",
+      "To store data",
+      "To manage hardware and software resourcese",
+      "To display images and videos"
+    ]
+  },
+];
